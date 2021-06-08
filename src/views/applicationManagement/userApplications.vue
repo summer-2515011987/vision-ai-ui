a09cf3fcab71106c83f33b1c6e49e91cc7d08098
mwk <2515011987@qq.com>
2021-06-08 11:16:56 +0800

    first commit

---
+++ src/views/applicationManagement/userApplications.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="userApplications">
+    用户应用
+  </div>
+</template>
+
+<script>
+export default {
+  name: "userApplications",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped lang="less"></style>
